8a16cca3821859c2edbf8aecfac29014f11c685e
John Imeobong <imeobongjohn38@gmail.com>
2025-04-28 15:46:22 +0100

    Add files via upload

---
+++ Practices.java
@@ -0,0 +1,16 @@
+public class Practices {
+public enum Purity {
+	GIRL, BEAUTIFUL, ANGRYBIRD, PHILANTROPIST, FUNNY, READYTOLEARN, OUTSPOKEN, INTELLIGENT
+}
+
+ public static void main(String[] args) {
+
+	Purity babyGirl = Purity.ANGRYBIRD;
+	System.out.print(babyGirl);
+
+
+}
+
+
+
+}
